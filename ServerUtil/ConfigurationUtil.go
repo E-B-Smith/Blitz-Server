ebb36037400cc86044afdc079ad56fa4eff32b1f
Edward B. Smith <smith.ed.b@gmail.com>
2016-06-20 19:05:29 -0700

    Squashed 'src/violent.blue/GoKit/' changes from d383aca..e6c1a59
    e6c1a59 Updated git-subtrees, fixed web page, updated html files.
    8de7a98 Added a resource semaphore.
    9c6aedc Page loads and parse work. Fixed Atlas Obscura stuff.
    815def9 Updated database and debugged load wiki.
    a954386 Fix makefile a do fixes as per tests.
    aa7c028 Updating command line tool.
    070c50b Documentation.
    042f4f8 .
    
    git-subtree-dir: src/violent.blue/GoKit
    git-subtree-split: e6c1a59abe254018f9a2181ea35470f67c22fb95

--- ServerUtil/ConfigurationUtil.go
+++ ServerUtil/ConfigurationUtil.go
@@ -53,9 +53,9 @@ func (config *Configuration) OpenConfig() error {
 
     Log.Startf("%s version %s pid %d compiled %s.",
         config.ServiceName,
-        CompileVersion(),
+        Util.CompileVersion(),
         os.Getpid(),
-        CompileTime(),
+        Util.CompileTime(),
     )
     Log.Debugf("Configuration: %+v.", config)
 
@@ -212,6 +212,10 @@ func (config *Configuration) RemovePIDFile() error {
 }
 
 
+func (config *Configuration) ServiceURL() string {
+    return config.ServerURL + config.ServicePrefix
+}
+
 
 //----------------------------------------------------------------------------------------
 //                                                           TCP Command & Control Channel
@@ -220,7 +224,7 @@ func (config *Configuration) RemovePIDFile() error {
 
 func (config *Configuration) ServerStatusString() string {
     pinfo, _ := Util.GetProcessInfo(os.Getpid())
-    result := fmt.Sprintf("%s:%d PID %d Elapsed %s CPU %1.1f%% Thr %d/%d Mem %s Messages: %s",
+    result := fmt.Sprintf("%16s:%d PID %d Elapsed %s CPU %1.1f%% Thr %d/%d Mem %s Messages: %s",
         config.ServiceName,
         config.ServicePort,
         pinfo.PID,
@@ -290,7 +294,7 @@ func ProcessTCPCommands(config *Configuration, connection net.Conn) {
                 case "hello":
                     _, error = connection.Write([]byte(">>> Hello.\n"))
                 case "version":
-                    s := fmt.Sprintf(">>> Software version %s.\n", CompileVersion())
+                    s := fmt.Sprintf(">>> Software version %s.\n", Util.CompileVersion())
                     _, error = connection.Write([]byte(s))
                 case "status":
                     s := fmt.Sprintf("%s.\n", config.ServerStatusString())
@@ -347,7 +351,6 @@ func (config *Configuration) StartTCPCommandChannel() {
         }
     } ()
 }
-
 
 
 //----------------------------------------------------------------------------------------
---
+++ Util/CompileTime.go
@@ -0,0 +1,22 @@
+
+
+//----------------------------------------------------------------------------------------
+//
+//                                                                          CompileTime.go
+//                                                                  Complile time and date
+//
+//                                                                    E.B.Smith, June 2016
+//                        -©- Copyright © 2014-2016 Edward Smith, all rights reserved. -©-
+//
+//----------------------------------------------------------------------------------------
+
+
+package Util
+
+
+var compileVersion              string = "0.0.0"
+var compileTime                 string = "Compile time not set"
+
+func CompileVersion() string    { return compileVersion }
+func CompileTime() string       { return compileTime }
+
